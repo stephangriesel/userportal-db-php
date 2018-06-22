51274290586d8b70638b88d5f3d17a88168b5150
Stephan Griesel <sgriesel@gmail.com>
2018-06-22 05:16:33 +0200

    add db connection

--- config.php
+++ config.php
@@ -3,4 +3,8 @@ $server = 'localhost';
 $user = 'root';
 $pass = '';
 $db = 'system';
+
+$conn = mysqli_connect($server,$user,$pass) or die("error");
+
+$selectdbb = mysqli_connect_db($conn,$db) or die ("error");
 ?>

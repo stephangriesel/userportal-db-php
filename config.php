05371eea17cdf1b3b9a8376899266c55ca145800
Stephan Griesel <sgriesel@gmail.com>
2018-06-25 20:54:32 +0200

    add register form

--- config.php
+++ config.php
@@ -6,5 +6,5 @@ $db = 'system';
 
 $conn = mysqli_connect($server,$user,$pass) or die("error");
 
-$selectdbb = mysqli_connect_db($conn,$db) or die ("error");
+$selectdbb = mysqli_select_db($conn,$db) or die ("error");
 ?>
---
+++ register.php
@@ -0,0 +1,23 @@
+<?php
+include 'config.php';
+
+if (isset($_POST["u_btn"])) {
+    $u_name = $_POST["u_name"];
+    $u_email = $_POST["u_name"];
+    $u_pass = $_POST["u_pass"];
+    if (empty($u_name) || empty($u_email) || empty ($u_pass) ) {
+        echo "Please complete all fields";
+    }
+}
+
+?>
+
+<form action="register.php" method="post">
+    <label>Name</label>
+    <input type="text" name="u_name" value=""><br />
+    <label>Email</label>
+    <input type="email" name="u_email" value=""><br />
+    <label>Password</label>
+    <input type="password" name="u_pass" value=""><br />
+    <input type="submit" name="u_btn" value="sign up"><br />
+</form>
